f861a6f7cd1d53bed4c6b971deff628d4ec90261
Diogo Souza Soares <diogo.soares@ativossa.com.br>
2023-08-28 18:00:50 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,13 @@
+//bolean
+//true -> verdadeiro
+//false -> falso
+const primeiroNumero = 5
+const segundoNumero = 10
+
+// console.log(segundoNumero === primeiroNumero)
+
+
+const texto1 = "Alura"
+const texto2 = "alura"
+
+console.log(texto1 === texto2) 
---
+++ type-string.js
@@ -0,0 +1,22 @@
+const texto1 = "hello World"
+const texto2 = "hello World"
+const senha = "senhaSuperSegura45631"
+const StringDeNumeros = "3216546"
+
+const citacao = "Meu nome è "
+const meunome = "Leonardo"
+
+
+console.log(citacao)
+
+// Concatenação formas <+>
+
+console.log(citacao + meunome)
+
+
+
+//template string ou template literal --- seria outra forma de escrever 
+
+
+
+
