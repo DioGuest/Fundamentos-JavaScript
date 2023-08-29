ec44838e9e3cdbcf4f2e37d569f4ec172258535e
Diogo Souza Soares <diogo.soares@ativossa.com.br>
2023-08-29 16:45:25 -0300

    codigos var conversoes

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dado
+// booleanos
+
+// conversão implicita
+const numero = 456
+const numeroStrig = Number("456")
+
+//Number()
+//String()
+console.log(numero == numeroStrig)
+console.log(numero + Number(numeroStrig))
+
+//conversão explicita
---
+++ truthy-falsy.js
@@ -0,0 +1,10 @@
+const usuarioLogado =  true;
+const contaPaga = false;
+
+// truthy ou falsy 
+
+// 0 => false 
+// 1 => true
+
+console.log(0 == false)
+console.log("" == false)
